ec803987a675134d3a6bb67b81ce594d5990d251
darya55k <darya55k2001@yandex.ru>
2021-09-12 12:44:23 +0300

    Revert "movies"
    This reverts commit c43a6e3b9b7b2a9d47a1f271ff58ee97ceb097e7.

--- errors/conflict-err.js
+++
@@ -1,8 +0,0 @@
-class ConflictErr extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 409;
-  }
-}
-
-module.exports = ConflictErr;
--- errors/unauthorized-err.js
+++
@@ -1,8 +0,0 @@
-class UnauthorizedErr extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
-
-module.exports = UnauthorizedErr;
